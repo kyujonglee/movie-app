714f42c0476fd685e43b02c8f5f0b298f4e411b2
이규종 <kyujong93@naver.com>
2019-07-05 18:55:01 +0900

    functional Component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React,{ Component }from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "Matrix",
-    poster:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWeFS2F8w7Y_JQKLA-dOBc5q5sLHsT3Q8H0PHQemqcpca4JWo"
-  },
-  {
-    title: "괴물",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EA%B4%B4%EB%AC%BC.jpg/250px-%EA%B4%B4%EB%AC%BC.jpg"
-  },
-  {
-    title: "어벤저스",
-    poster:
-      "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/23Y6/image/Z3eB-5i-2QIg9jXAhpsP86jc090.jpg"
-  },
-  {
-    title: "어벤저스 인피니트 워",
-    poster:
-      "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2018%2F07%2Favengers-infinity-war-2018-1.jpg?q=75&w=800&cbr=1&fit=max"
-  }
-];
-
 class App extends Component {
   // Render : componentWillMount() => render() -> componentDidMount()
+  state = {};
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Matrix",
+            poster:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWeFS2F8w7Y_JQKLA-dOBc5q5sLHsT3Q8H0PHQemqcpca4JWo"
+          },
+          {
+            title: "괴물",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EA%B4%B4%EB%AC%BC.jpg/250px-%EA%B4%B4%EB%AC%BC.jpg"
+          },
+          {
+            title: "어벤저스",
+            poster:
+              "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/23Y6/image/Z3eB-5i-2QIg9jXAhpsP86jc090.jpg"
+          },
+          {
+            title: "어벤저스 인피니트 워",
+            poster:
+              "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2018%2F07%2Favengers-infinity-war-2018-1.jpg?q=75&w=800&cbr=1&fit=max"
+          },
+          {
+            title: "기생충",
+            poster:
+              "http://image.chosun.com/sitedata/image/201906/24/2019062401560_0.png"
+          }
+        ]
+      });
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => (
+      <Movie title={movie.title} poster={movie.poster} key={index} />
+    ));
+    return movies;
+  };
 
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => (
-          <Movie title={movie.title} poster={movie.poster} key={index} />
-        ))}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,36 +1,31 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
+import React from "react";
+import propTypes from "prop-types";
+import "./Movie.css";
 
-class Movie extends Component {
-
-    static propTypes = {
-        title : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-    render() {
-        // console.log(this.props.poster);
-        return (
-            <div>
-                <MoviePoster potato={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component{
-    
-    static proTypes = {
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        return (
-            <img src={this.props.potato} />
-        )
-    }
+// functional component
+// stateless component
+// state가 필요없고 props만 존재할 때 이를 stateless component 라 함.
+// function render 존재 x , 라이프 사이클 존재 x
+function MoviePoster({ poster }) {
+  return <img src={poster} alt="Movie Poster" />;
 }
 
-export default Movie
+Movie.propTypes = {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
+  };
+
+MoviePoster.propTypes = {
+  poster: propTypes.string.isRequired
+};
+
+export default Movie;
