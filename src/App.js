284d31798c19b7709ad7cc8bcd2b609126e24259
이규종 <kyujong93@naver.com>
2019-07-08 03:24:15 +0900

    before css

--- src/App.js
+++ src/App.js
@@ -7,44 +7,34 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGWeFS2F8w7Y_JQKLA-dOBc5q5sLHsT3Q8H0PHQemqcpca4JWo"
-          },
-          {
-            title: "괴물",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EA%B4%B4%EB%AC%BC.jpg/250px-%EA%B4%B4%EB%AC%BC.jpg"
-          },
-          {
-            title: "어벤저스",
-            poster:
-              "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/23Y6/image/Z3eB-5i-2QIg9jXAhpsP86jc090.jpg"
-          },
-          {
-            title: "어벤저스 인피니트 워",
-            poster:
-              "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2018%2F07%2Favengers-infinity-war-2018-1.jpg?q=75&w=800&cbr=1&fit=max"
-          },
-          {
-            title: "기생충",
-            poster:
-              "http://image.chosun.com/sitedata/image/201906/24/2019062401560_0.png"
-          }
-        ]
-      });
-    }, 5000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => (
-      <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => (
+      <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        key={movie.id}
+      />
     ));
     return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.lt/api/v2/list_movies.json?sort_by=like_count")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
 
   render() {
--- src/Movie.js
+++ src/Movie.js
@@ -2,11 +2,21 @@ import React from "react";
 import propTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ title, poster }) {
+function Movie({ title, poster, genres, synopsis }) {
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movies__Columns">
+        <MoviePoster poster={poster} alt={title}/>
+      </div>
+      <div className="Movies__Columns">
+        <h1>{title}</h1>
+        <div className="Movies__Genres">
+          {genres.map((genre, index) => (
+            <MovieGenre genre={genre} key={index} />
+          ))}
+        </div>
+        <p className="Movie__Synopsis">{synopsis}</p>
+      </div>
     </div>
   );
 }
@@ -15,17 +25,24 @@ function Movie({ title, poster }) {
 // stateless component
 // state가 필요없고 props만 존재할 때 이를 stateless component 라 함.
 // function render 존재 x , 라이프 사이클 존재 x
-function MoviePoster({ poster }) {
-  return <img src={poster} alt="Movie Poster" />;
+function MoviePoster({ poster,alt }) {
+  return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie__Genre">{genre} </span>;
 }
 
 Movie.propTypes = {
-    title: propTypes.string.isRequired,
-    poster: propTypes.string.isRequired
-  };
+  title: propTypes.string.isRequired,
+  poster: propTypes.string.isRequired,
+  genres: propTypes.array.isRequired,
+  synopsis: propTypes.string.isRequired
+};
 
 MoviePoster.propTypes = {
-  poster: propTypes.string.isRequired
+  poster: propTypes.string.isRequired,
+  alt : propTypes.string.isRequired
 };
 
 export default Movie;
