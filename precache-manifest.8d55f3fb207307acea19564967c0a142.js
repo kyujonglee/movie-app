318beda2ce5c5d1fc34c78a17c7e7dc586622184
이규종 <kyujong93@naver.com>
2019-07-09 02:54:27 +0900

    Updates

---
+++ precache-manifest.8d55f3fb207307acea19564967c0a142.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2db7f64696f5eaa83c2f02e03648ef2",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "c7a69c9c78d7625ae201",
+    "url": "/movie-app/static/js/2.ead80391.chunk.js"
+  },
+  {
+    "revision": "2462b3b9252a2831cd08",
+    "url": "/movie-app/static/js/main.0893468f.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-app/precache-manifest.6739f3511ba8606a8a9b199f31977ef5.js"
+  "/movie-app/precache-manifest.8d55f3fb207307acea19564967c0a142.js"
 );
 
 self.addEventListener('message', (event) => {
