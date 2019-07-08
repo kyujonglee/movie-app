e5a5bf85d30a2c8d615af33f82d080c9326ccf02
이규종 <kyujong93@naver.com>
2019-07-09 02:57:26 +0900

    css update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import React, { Component } from "react";
-import "./App.css";
 import Movie from "./Movie";
+import axios from "axios";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+// 추가해야 할 것!
+// 1. css 적용(Style Component 인강을 들은 후!) (Ok)
+// 2. 무한스크롤 적용
+// 3. fetch 대신해서 Axios 적용 (Ok)
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  a{
+    text-decoration: none;
+    color:inherit;
+  };
+  *{
+    box-sizing:border-box;
+  };
+`;
+
+const MovieApp = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #dcdde1;
+  padding: 100px 180px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 250px;
+  grid-gap: 50px;
+`;
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  width: 100%;
+  height: 100vh;
+`;
 
 class App extends Component {
   // Render : componentWillMount() => render() -> componentDidMount()
@@ -31,17 +69,22 @@ class App extends Component {
   };
 
   _callApi = () => {
-    return fetch("https://yts.lt/api/v2/list_movies.json?sort_by=like_count")
-      .then(response => response.json())
-      .then(json => json.data.movies)
+    return axios
+      .get("https://yts.lt/api/v2/list_movies.json?sort_by=like_count")
+      .then(json => json.data.data.movies)
       .catch(err => console.log(err));
   };
 
   render() {
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading"}
-      </div>
+      <>
+          {this.state.movies ? (
+            <MovieApp>{this._renderMovies()}</MovieApp>
+          ) : (
+            <Loading> Loading </Loading>
+          )}
+        <GlobalStyle />
+      </>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,82 @@
 import React from "react";
 import propTypes from "prop-types";
-import "./Movie.css";
+import styled from "styled-components";
+import LinesEllipsis from "react-lines-ellipsis";
+
+const Container = styled.div`
+  width: 600px;
+  display: grid;
+  grid-template-columns: 3.5fr 6.5fr;
+  background-color: white;
+  position: relative;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+`;
+
+const Poster = styled.img`
+  position: absolute;
+  top: -20px;
+  left: 20px;
+  display: block;
+  width: 180px;
+  height: 240px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+`;
+
+const Title = styled.span`
+  display: inline-block;
+  color: #2d3436;
+  font-size: 32px;
+  margin-top: 10px;
+  padding: 5px 0px;
+  font-weight: 600;
+`;
+
+const Genres = styled.span`
+  display: flex;
+  color: #b2bec3;
+  font-size: 16px;
+  & > span:not(:last-child) {
+    display: inline-block;
+    margin-right: 5px;
+  }
+`;
+
+const Columns = styled.div`
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  & > span {
+    padding: 10px 0px;
+  }
+`;
+const Synopsis = styled.span`
+  color: #b2bec3;
+`;
 
 function Movie({ title, poster, genres, synopsis }) {
   return (
-    <div className="Movie">
-      <div className="Movies__Columns">
-        <MoviePoster poster={poster} alt={title}/>
-      </div>
-      <div className="Movies__Columns">
-        <h1>{title}</h1>
-        <div className="Movies__Genres">
+    <Container>
+      <Columns>
+        <MoviePoster poster={poster} alt={title} />
+      </Columns>
+      <Columns>
+        <Title>{title}</Title>
+        <Genres>
           {genres.map((genre, index) => (
             <MovieGenre genre={genre} key={index} />
           ))}
-        </div>
-        <p className="Movie__Synopsis">{synopsis}</p>
-      </div>
-    </div>
+        </Genres>
+        <Synopsis>
+          <LinesEllipsis
+            text={synopsis}
+            maxLine="3"
+            ellipsis=" ..."
+            trimRight
+            basedOn="letters"
+          />
+        </Synopsis>
+      </Columns>
+    </Container>
   );
 }
 
@@ -25,12 +84,12 @@ function Movie({ title, poster, genres, synopsis }) {
 // stateless component
 // state가 필요없고 props만 존재할 때 이를 stateless component 라 함.
 // function render 존재 x , 라이프 사이클 존재 x
-function MoviePoster({ poster,alt }) {
-  return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
+function MoviePoster({ poster, alt }) {
+  return <Poster src={poster} alt={alt} title={alt} />;
 }
 
 function MovieGenre({ genre }) {
-  return <span className="Movie__Genre">{genre} </span>;
+  return <span>{genre}</span>;
 }
 
 Movie.propTypes = {
@@ -42,7 +101,30 @@ Movie.propTypes = {
 
 MoviePoster.propTypes = {
   poster: propTypes.string.isRequired,
-  alt : propTypes.string.isRequired
+  alt: propTypes.string.isRequired
 };
+
+// function Movie({ title, poster, genres, synopsis }) {
+//   return (
+//     <div className="Movie">
+//       <div className="Movies__Columns">
+//         <MoviePoster poster={poster} alt={title}/>
+//       </div>
+//       <div className="Movies__Columns">
+//         <h1>{title}</h1>
+//         <div className="Movies__Genres">
+//           {genres.map((genre, index) => (
+//             <MovieGenre genre={genre} key={index} />
+//           ))}
+//         </div>
+//         <p className="Movie__Synopsis">{synopsis}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function MoviePoster({ poster,alt }) {
+//   return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
+// }
 
 export default Movie;
